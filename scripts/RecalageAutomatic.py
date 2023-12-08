78ec3e15bdc84cea6d798e1e638026ecce85b8cb
Julie Breuil <jbpub@posteo.net>
2023-12-08 11:24:51 +0100

    supp IBSR.mgz and RecalageAuto adapted to new folder data

--- scripts/RecalageAutomatic.py
+++ scripts/RecalageAutomatic.py
@@ -3,21 +3,23 @@ import subprocess
 import SimpleITK as sitk
 
 # Liste des images dans les dossiers
-moving_images = os.listdir('moving_images')
-fixed_images = os.listdir('fixed_images')
+moving_images = os.listdir('/home/julie/Documents/SIR/projet_SIR/data/moving_images')
+print(moving_images)
+fixed_images = os.listdir('/home/julie/Documents/SIR/projet_SIR/data/fixed_images')
+print(fixed_images)
 
-files = os.listdir('parameters')
+files = os.listdir('/home/julie/Documents/SIR/projet_SIR/data/parameters')
 print(files)
 nbParam = len(files)
 if nbParam == 0:
     print("Aucun fichier de paramètres")
     exit()
 elif nbParam == 1:
-    parameters = os.path.join('parameters', files[0])
+    parameters = os.path.join('/home/julie/Documents/SIR/projet_SIR/data/parameters', files[0])
     print(parameters)
 elif nbParam == 2:
-    parameters_file1 = os.path.join('parameters', files[0])
-    parameters_file2 = os.path.join('parameters', files[1])
+    parameters_file1 = os.path.join('/home/julie/Documents/SIR/projet_SIR/data/parameters', files[0])
+    parameters_file2 = os.path.join('/home/julie/Documents/SIR/projet_SIR/data/parameters', files[1])
     print(parameters_file1)
     print(parameters_file2)
 else:
@@ -32,7 +34,7 @@ for moving_image in moving_images:
         fixed_number = fixed_image[5:7]
 
         # Créer le nom du dossier de sortie
-        output_folder = f"output{moving_number}{fixed_number}"
+        output_folder = f"/home/julie/Documents/SIR/projet_SIR/output/output{moving_number}{fixed_number}"
         os.makedirs(output_folder, exist_ok=True)
 
         # Créer le nom du dossier de résultat
@@ -41,11 +43,11 @@ for moving_image in moving_images:
 
         # Construire les commandes elastix et transformix
         if nbParam == 1:
-            elastix_command = f"elastix -m moving_images/{moving_image} -f fixed_images/{fixed_image} -p {parameters} -out {output_folder}"
+            elastix_command = f"elastix -m /home/julie/Documents/SIR/projet_SIR/data/moving_images/{moving_image} -f /home/julie/Documents/SIR/projet_SIR/data/fixed_images/{fixed_image} -p {parameters} -out {output_folder}"
         if nbParam == 2:
-            elastix_command = f"elastix -m moving_images/{moving_image} -f fixed_images/{fixed_image} -p {parameters_file1} -p {parameters_file2} -out {output_folder}"
+            elastix_command = f"elastix -m /home/julie/Documents/SIR/projet_SIR/data/moving_images/{moving_image} -f /home/julie/Documents/SIR/projet_SIR/data/fixed_images/{fixed_image} -p {parameters_file1} -p {parameters_file2} -out {output_folder}"
         #elastix -f fixed_image.nii -m moving_image.nii -out output_folder -p parametres1.txt,parametres2.txt
-        transformix_command = f"transformix -in seg_images/IBSR_{fixed_number}_seg_ana.nii -tp {output_folder}/TransformParameters.0.txt -out {result_folder}"
+        transformix_command = f"transformix -in /home/julie/Documents/SIR/projet_SIR/data/seg_images/IBSR_{fixed_number}_seg_ana.nii -tp {output_folder}/TransformParameters.0.txt -out {result_folder}"
 
         # Exécuter les commandes
         subprocess.run(elastix_command, shell=True)
