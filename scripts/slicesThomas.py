56e6f40bee4717ea6250f7888222103b66d158da
thomas <thomas.feutren@insa-lyon.fr>
2023-12-27 18:26:23 +0100

    Création des coupes fichiers CSV
    J'ai le script python qui prends en paramètre le dossier qui contient toutes les images résultantes et génère un fichier CSV pour chaques images

--- scripts/slicesThomas.py
+++ scripts/slicesThomas.py
@@ -1,6 +1,6 @@
 """ 
 Auteur : Thomas
-Récuprérer des informations sur une coupe d'une image résutlantes :
+Récuprérer des informations sur une coupe des images résutlantes à partir d'un dossier contenant des images au format nii.gz. :
 
 - Le type de l'image mobile (FLAIR, T1, T2)
 - L'image fixe utilisée (IBSR_01, IBSR_02, etc.)
@@ -11,83 +11,85 @@ Récuprérer des informations sur une coupe d'une image résutlantes :
 # --------------------------------------------------------------------------------
 # Import
 
+import csv
 import sys
+import os
 import numpy as np
 import SimpleITK as sitk
-import matplotlib.pyplot as plt
 
 # --------------------------------------------------------------------------------
 # Récupérer l'argument
 
 if len(sys.argv) != 2:
-    print("Usage: python3 mon_script.py <inputFile>")
+    print("Usage: python3 mon_script.py <inputFolder>")
     sys.exit(1)
 
-inputFile = sys.argv[1]
+inputFolder = sys.argv[1]
 
 # --------------------------------------------------------------------------------
-# Lire l'image
+# Liste des fichiers dans le dossier
 
-image = sitk.ReadImage(inputFile)
+inputFiles = [f for f in os.listdir(inputFolder) if f.endswith('.nii.gz')]
 
 # --------------------------------------------------------------------------------
-# Informations sur l'image
+# Traitement de chaque fichier dans le dossier
 
-size = image.GetSize()
+for inputFile in inputFiles:
 
-if "FLAIR" in inputFile:
-    typeIRM = "FLAIR"
-elif "OAS" in inputFile:
-    typeIRM = "T1"
-elif "T2" in inputFile:
-    typeIRM = "T2"
-else :
-    typeIRM = "none"
+    inputFilePath = os.path.join(inputFolder, inputFile)
+    image = sitk.ReadImage(inputFilePath)
+    size = image.GetSize()
 
-for i in range(1,19):
-    if i < 10:
-        i = "0" + str(i)
+    if "FLAIR" in inputFile:
+        typeIRM = "FLAIR"
+    elif "OAS" in inputFile:
+        typeIRM = "T1"
+    elif "T2" in inputFile:
+        typeIRM = "T2"
     else:
-        i = str(i)
-    if f"IBSR_{i}_" in inputFile:
-        fixedImagePath = f"../Data/IBSR_fixed_images/seg/IBSR_{i}_seg_ana.nii.gz"
+        typeIRM = "none"
 
-fixedImage = sitk.ReadImage(fixedImagePath)
-arrayFixedImage = sitk.GetArrayFromImage(fixedImage)
+    for i in range(1, 19):
+        if i < 10:
+            i = "0" + str(i)
+        else:
+            i = str(i)
+        if f"IBSR_{i}_" in inputFile:
+            fixedImagePath = f"../Data/IBSR_fixed_images/seg/IBSR_{i}_seg_ana.nii.gz"
 
-# --------------------------------------------------------------------------------
-# Afficher informations image
-    
-print(f"type de l'IRM : {typeIRM}")
-print(f"Image fixe : {fixedImagePath}")
+    fixedImage = sitk.ReadImage(fixedImagePath)
+    arrayFixedImage = sitk.GetArrayFromImage(fixedImage)
 
-# --------------------------------------------------------------------------------
-# Afficher informations coupes
+    sagittal_len = size[0]
+    coronal_len = size[1]
+    axial_len = size[2]
 
-sagittal_len = size[0]
-coronal_len = size[1]
-axial_len = size[2]
+    sliceArray = []
 
-sliceArray = []
+    for i in range(coronal_len):
+        image_slice = arrayFixedImage[i, :, :]
+        unique_values, counts = np.unique(image_slice, return_counts=True)
+        resultats_comptage = list(zip(unique_values, counts))
+        sliceArray.append(["Coronal", i, resultats_comptage])
 
-for i in range(coronal_len):
-    image_slice = arrayFixedImage[i, :, :]
-    unique_values, counts = np.unique(image_slice, return_counts=True)
-    resultats_comptage = list(zip(unique_values, counts))
-    sliceArray.append(["Coronal", i, resultats_comptage])
+    for i in range(axial_len):
+        image_slice = arrayFixedImage[:, i, :]
+        unique_values, counts = np.unique(image_slice, return_counts=True)
+        resultats_comptage = list(zip(unique_values, counts))
+        sliceArray.append(["Axial", i, resultats_comptage])
 
-for i in range(axial_len):
-    image_slice = arrayFixedImage[:, i, :]
-    unique_values, counts = np.unique(image_slice, return_counts=True)
-    resultats_comptage = list(zip(unique_values, counts))
-    sliceArray.append(["Axial", i, resultats_comptage])
+    for i in range(sagittal_len):
+        image_slice = arrayFixedImage[:, :, i]
+        unique_values, counts = np.unique(image_slice, return_counts=True)
+        resultats_comptage = list(zip(unique_values, counts))
+        sliceArray.append(["Sagittal", i, resultats_comptage])
 
-for i in range(sagittal_len):
-    image_slice = arrayFixedImage[:, :, i]
-    unique_values, counts = np.unique(image_slice, return_counts=True)
-    resultats_comptage = list(zip(unique_values, counts))
-    sliceArray.append(["Sagittal", i, resultats_comptage])
+    # --------------------------------------------------------------------------------
+    # Enregistrer au format CSV
+    csv_file = os.path.join(inputFolder, f"{inputFile[:-7]}.csv")
 
-print(sliceArray)
+    with open(csv_file, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerows(sliceArray)
 
-
+    print(f"Le fichier {inputFile} a été traité et le CSV a été enregistré dans {csv_file}.")
