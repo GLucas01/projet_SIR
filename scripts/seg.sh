2d6ac656b91f19dc33e9536612baae40e65ece57
Emmakki <emmivdc@gmail.com>
2024-02-01 16:28:00 +0100

    Update seg.sh

--- scripts/seg.sh
+++ scripts/seg.sh
@@ -8,30 +8,36 @@ if [ "$#" -ne 4 ]; then
     exit 1
 fi
 
-input_seg_folder="$1" # fixed/seg
+input_seg_folder="$1" # moving/seg
 output_folder="$2" # moving_fixed/seg_inv
-brain_folder="$3" # moving/brain
+brain_folder="$3" # fixed/brain
 brain_reg_folder="$4" # moving_fixed/reg_brain
 
-
+#nb_file=0
 for input_image in "${brain_folder}"/*.nii.gz; do
-    moving_name=$(basename "${input_image}" .nii.gz)
+    fixed_name=$(basename "${input_image}" .nii.gz)
+    #echo "${fixed_name}"
 
     for seg_image in "${input_seg_folder}"/*.nii.gz; do
-        seg_name=$(basename "${seg_image}")
+        seg_name=$(basename "${seg_image}" .nii.gz)
+        #echo "${seg_name}"
 
-        fixed_brain_name=$(echo "${seg_name}" | sed 's/_majority.nii.gz/_brain.nii.gz/')
-        fixed_matrix_name=$(echo "${seg_name}" | sed 's/_majority.nii.gz/_brain0GenericAffine.mat/')
+        moving_reg_name=$(echo "${seg_name}" | sed 's/_seg_ana/_ana/')
+        #echo "${moving_reg_name}"
+        matrix_name="${fixed_name}0GenericAffine.mat"
+        #echo "${matrix_name}"
 
-        output_image="${output_folder}/${moving_name}_seg_${seg_name}"
-        reg_matrix="${brain_reg_folder}/${moving_name}_reg_${fixed_matrix_name}"
+        output_image="${output_folder}/${moving_reg_name}_seg_${fixed_name}.nii.gz"
+        reg_matrix="${brain_reg_folder}/${moving_reg_name}_reg_${matrix_name}"
+        #echo "${reg_matrix}"
+        #nb_file=$((nb_file + 1))
         
         if [ -e "$output_image" ]; then
             echo "Skipping transformation for $moving_name. Output file already exists: $output_image"
             
         else
 
-            /media/emma/DALLE_BRAIN/SIR/script/ANTS/antsApplyTransforms -d 3 \
+            /home/emma/Documents/SIR/script/ANTS/antsApplyTransforms -d 3 \
                -i "${seg_image}" \
                -r "${input_image}" \
                -o "${output_image}" \
@@ -39,6 +45,8 @@ for input_image in "${brain_folder}"/*.nii.gz; do
                --interpolation NearestNeighbor
 
         echo "Fichier créé : ${output_image}"
+        fi
     done
 done
+#echo "$nb_file"
 
